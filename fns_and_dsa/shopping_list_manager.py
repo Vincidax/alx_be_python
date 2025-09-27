f748f92ac2bc4e886897f1dd10fb773ea3b93fa5
vincidax <vincentdx18@gmail.com>
2025-09-27 19:03:18 +0200

    updating shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -5,7 +5,7 @@ A simple Shopping List Manager using Python lists.
 """
 
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
