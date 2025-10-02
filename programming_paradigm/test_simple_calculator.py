8d99ab6c12c6d14f4e7aee2c776a083db2344b3f
vincidax <vincentdx18@gmail.com>
2025-10-02 22:17:19 +0200

    updating test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,26 +2,30 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Set up the self.calc instance before each test."""
+        self.calc = SimpleCalculator()
+
     def test_add(self):
         """Test the add method."""
-        self.assertEqual(SimpleCalculator().add(2, 3), 5)
-        self.assertEqual(SimpleCalculator().add(-2, 3), 1)
-        self.assertEqual(SimpleCalculator().add(2, -3), -1)
-        self.assertEqual(SimpleCalculator().add(-2, -3), -5)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-2, 3), 1)
+        self.assertEqual(self.calc.add(2, -3), -1)
+        self.assertEqual(self.calc.add(-2, -3), -5)
 
     def test_subtract(self):
-        self.assertEqual(SimpleCalculator().subtract(2, 3), -1)
-        self.assertEqual(SimpleCalculator().subtract(3, 2), 1)
-        self.assertEqual(SimpleCalculator().subtract(3, -2), 5)
-        self.assertEqual(SimpleCalculator().subtract(-3, 2), 5)
-        self.assertEqual(SimpleCalculator().subtract(-3, -2), -1)
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(3, -2), 5)
+        self.assertEqual(self.calc.subtract(-3, 2), -5)
+        self.assertEqual(self.calc.subtract(-3, -2), -1)
 
     def test_multiply(self):
-        self.assertEqual(SimpleCalculator().multiply(2, 3), 6)
-        self.assertEqual(SimpleCalculator().multiply(2, 0), 0)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(2, 0), 0)
 
     def test_divide_normal(self):
-        self.assertEqual(SimpleCalculator().divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(6, 3), 2)
 
     def test_divide_by_zero(self):
-        self.assertIsNone(SimpleCalculator().divide(10, 0))
+        self.assertIsNone(self.calc.divide(10, 0))
