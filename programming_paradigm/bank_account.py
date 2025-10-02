df13c56fadb45c150366d32c81995eeecc08269f
vincidax <vincentdx18@gmail.com>
2025-10-02 09:52:09 +0200

    editing bank_account.py in programming_paradigm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,4 +1,4 @@
-class BankAccount():
+class BankAccount:
     def __init__(self, account_balance):
         """Initialize the account with an optional starting balance."""
         self.account_balance= account_balance
