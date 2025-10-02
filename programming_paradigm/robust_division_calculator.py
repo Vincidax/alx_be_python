eb17e6624d3385ee68acb450b627a8fc62543254
vincidax <vincentdx18@gmail.com>
2025-10-02 21:17:40 +0200

    adding robust_calculator.py

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,10 @@
+def safe_divide(numerator, denominator):
+    try:
+        result = int(numerator) / int(denominator)
+        return result
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        return None
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+        return None
